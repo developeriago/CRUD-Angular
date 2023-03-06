37f66aa1fdbbafcba33c9ccb6e72cbc511c2da0f
Iago Almeida <iagoalmeidadev@outlook.com>
2023-03-06 00:30:47 -0300

    Adicionado poupap para o tratamento de erros

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { CoursesService } from './../services/courses.service';
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
-import { Observable } from 'rxjs'
+import { catchError, Observable, of } from 'rxjs'
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -15,10 +17,22 @@ export class CoursesComponent {
 
 
 
-  constructor(private coursesServices: CoursesService){
+  constructor(private coursesServices: CoursesService, public dialog: MatDialog){
 
-    this.courses$ = this.coursesServices.list();
+    this.courses$ = this.coursesServices.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar os cursos.');
+        return of([])
+      })
+    );
 
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit():void{
